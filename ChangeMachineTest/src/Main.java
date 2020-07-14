73d8b5c8729368f1fabfdf725ef111d615d4f089
AttilaWiedemann <attila.wiedemann@gmail.com>
2020-07-14 15:38:29 +0200

    adds test version euro and huf

--- ChangeMachineTest/src/Main.java
+++ ChangeMachineTest/src/Main.java
@@ -4,8 +4,10 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		int[] hufValues = {5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000};
+		int[] euroValues = {1, 2, 5, 10, 20, 50, 100, 200};
 		Money forint = new Money("huf", hufValues);
-		System.out.println(forint.changer(100001));
+		Money euro = new Money("eur", euroValues);
+		System.out.println(euro.changer(100001));
 	}
 
 }
